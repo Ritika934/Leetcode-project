8ddfb21667f148255c8a371e7df3546f797233fe
Ritika Parmar <parmarritika9878@gmail.com>
2025-07-29 19:20:27 +0530

    Update axiosclient.js

--- Frontend/axiosclient.js
+++ Frontend/axiosclient.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosclient = axios.create({
 
-    baseURL:'https://leetcode-project-backend.vercel.app',
+    baseURL:'https://leetcode-project-backend-3s5d.onrender.com',
     withCredentials:true,
     headers:{
         'Content-Type':'application/json'
