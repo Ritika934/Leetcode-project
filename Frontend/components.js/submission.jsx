bda5ea8c2b908cb3879b7e84781818bea66a3f35
Ritika Parmar <parmarritika9878@gmail.com>
2025-07-30 23:23:33 +0530

    Update submission.jsx

--- Frontend/components.js/submission.jsx
+++ Frontend/components.js/submission.jsx
@@ -62,7 +62,7 @@ const SubmissionHistory = ({ problemId }) => {
     if (loading) {
         return (
             <div className="flex justify-center items-center h-64">
-                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-yellow-400"></div>
+                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-primary"></div>
             </div>
         );
     }
