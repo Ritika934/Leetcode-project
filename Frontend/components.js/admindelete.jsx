d1f61c00e4bddc58cda1d84b6f2fb0354c6bf670
Ritika Parmar <parmarritika9878@gmail.com>
2025-08-03 17:46:47 +0530

    Update admindelete.jsx

--- Frontend/components.js/admindelete.jsx
+++ Frontend/components.js/admindelete.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom"; // 1. Import useNavigate
+import { useNavigate } from "react-router"; // 1. Import useNavigate
 import axiosclient from "../axiosclient";
 
 function Admindelete() {
